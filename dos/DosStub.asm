364211389ea9a9fed9829732d16413b20623a531
unknown <hamidreza.ebtehaj@gmail.com>
2018-05-20 01:44:26 +0200

    DOS stub is done.

--- dos/DosStub.asm
+++ dos/DosStub.asm
@@ -1,24 +1,40 @@
-segment code 
+;=============================================================================
+; @file DosStub.asm
+;
+; It's the program that would be a replacement for the DOS stub.
+; It shows the message 'This program cannot be run in DOS mode after 1am.'
+; and jumps to the CrackMe.asm which contains pe32 code.	
+;
+; Copyright 2017 Hamidreza Ebtehaj.
+; Use of this source code is governed by a BSD-style license that can
+; be found in the LICENSE file.
+;=============================================================================
+
+OFFSET_START	equ	0x350
+
+segment code
+; Going to be a 16 bit application
+bits 16
 
 ..start:
-	jmp		$
-	push	ax
-	push	bx
+	;Setting up segment registers.
 	mov     ax,data 
 	mov     ds,ax 
 	mov     ax,stack 
 	mov     ss,ax 
 	mov     sp,stacktop
+
+	;Writing 'cannot run in DOS' message.
 	mov     dx,hello 
 	mov     ah,9 
 	int     0x21
-	mov     ax,0x4c00 
-	int     0x21
 
+	; OK This works for a jump inside text section of pe file.
+	jmp		$$ + OFFSET_START
 segment data 
 
 hello:  db      'This program cannot be run in DOS mode after 1am.', 13, 10, '$'
 
 segment stack stack 
-	resb 64 
+	resb 0x100 
 stacktop:
--- win/CrackMe.asm
+++ win/CrackMe.asm
@@ -1,9 +1,15 @@
-SECTION		.data
+;=============================================================================
+; @file CrackMe.asm
+;
+; It's the 32 bit main pe. However it contains the code that is going to be executed
+; when the execution is passed from dos stub.
+;
+; Copyright 2017 Hamidreza Ebtehaj.
+; Use of this source code is governed by a BSD-style license that can
+; be found in the LICENSE file.
+;=============================================================================
 
-Message:	db      'Hello World!', 10
-MsgLen:         equ     $-Message
-
-SECTION         .bss
+%include "include\scripts.inc"
 
 SECTION         .text
 
@@ -12,7 +18,14 @@ SECTION         .text
 	extern  _ExitProcess@4
 
 	global _main
-	
+
+; Here is the place that DOS application will be continued.
+bits 16
+_main16:
+	call CheckTime
+
+
+bits 32
 _main:
 	; DWORD  bytes;    
 	mov     ebp, esp
@@ -38,3 +51,16 @@ _main:
 
 	; never here
 	hlt
+
+bits 16
+CheckTime:
+	jmp $
+	ret
+	
+SECTION		.data
+; RC4TABLE - it is used in both in win32 and dos apps
+RC4TableSignature:	db	0xde,0xad,0xbe,0xef
+rc4table
+
+Message:	db      'Hello World!', 10
+MsgLen:		equ     $-Message
